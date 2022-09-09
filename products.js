43408e87c9c47d246dcfd446fcde6aa7ef5398a4
vamsibatchu8 <52702855+vamsibatchu8@users.noreply.github.com>
2022-09-09 14:56:24 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,41 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Asus ROG Strix G15 Ryzen ",
+    desc : "Gaming Laptop  (15.6 inch, Eclipse Gray, 2.10 kg, With MS Office)",
+    img : "1.png",
+    price : 89999
+  },
+  124: {
+    name : "acer Nitro 5 Ryzen ",
+    desc : "Gaming Laptop  (15.6 inch, Black, 2.4 kg)",
+    img : "2.png",
+    price : 96990
+  },
+  125: {
+    name : "Lenovo IdeaPad 3 Ryzen 5 ",
+    desc : "Thin and Light Laptop  (15.6 inch, Arctic Grey, 1.65 kg, With MS Office)",
+    img : "3.png",
+    price : 45990
+  },
+  126: {
+    name : "HP OMEN Ryzen 7 ",
+    desc : "Gaming Laptop  (16.1 Inch, Mica Silver, 2.32 Kg, With MS Office) ",
+    img  : "4.png",
+    price : 98990
+  },
+
+  127:{
+    name : "SAREGAMA Carvaan Mini Bengali ",
+    desc : " 5 W Bluetooth Home Theatre",
+    img  : "6.png",
+    price : 2091
+  },
+
+  128:{
+    name : "APPLE Airpods Pro",
+    desc : "MagSafe Charging Case Bluetooth Headset  (White, True Wireless)",
+    img  : "7.png",
+    price : 19900
+  }
+};
